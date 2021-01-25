700c1d18789eccb3bc345d392001861694eb153c
Hannah Desmond <desmond.hannah@googlemail.com>
2021-01-25 15:38:44 +0000

    Return students by cohort.

--- directory-exercises.rb
+++ directory-exercises.rb
@@ -7,7 +7,7 @@ def input_students
     while true do
       puts "What is the student's cohort?"
       cohort_string = gets.chomp
-      puts "Is this correct? #{cohort_string}. Put y or n"
+      puts "Is this correct? #{cohort_string}? Put y or n"
       answer = gets.chomp
       if answer == 'y'
       puts "What is the student's hobby?"
@@ -20,6 +20,7 @@ def input_students
     students << {name: name, cohort: cohort_string.to_sym,
       hobby: hobby, country: country}
     puts "Now we have #{students.count} students"
+    puts "What is the name of the next student?"
     name = gets.chomp
   end
   students
@@ -29,7 +30,26 @@ def print_header
   puts "The students of Villains Academy".center(50)
   puts "--------------".center(50)
 end
+
 def print(students)
+  students_array = []
+  puts "Which cohort do you want to print?"
+  input = gets.chomp
+  students.each do |student_hash|
+    student_hash.each do |key, value|
+      if key == :cohort && value == input.to_sym
+        students_array.push(student_hash[:name])
+      end
+    end
+  end
+  puts students_array.join(", ")
+end
+ # There is an array
+ # Ask user for the cohort they would like to see
+ # if the hash contains the desired cohort, print the name
+
+
+=begin
   counter = 0
   while true do
     hash = students[counter]
@@ -41,7 +61,9 @@ def print(students)
       break
     end
   end
-end
+=end
+
+
 def print_footer(students)
   puts "Overall, we have #{students.count} great students".center(50)
 end
