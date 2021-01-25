3f9c56ae8c64b6a928e2a710bfcf32a144f62e89
Hannah Desmond <desmond.hannah@googlemail.com>
2021-01-25 14:52:31 +0000

    Ask for hobby and country

--- directory-exercises.rb
+++ directory-exercises.rb
@@ -4,23 +4,46 @@ def input_students
   students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
+    while true do
+      puts "What is the student's cohort?"
+      cohort_string = gets.chomp
+      puts "Is this correct? #{cohort_string}. Put y or n"
+      answer = gets.chomp
+      if answer == 'y'
+      puts "What is the student's hobby?"
+      hobby = gets.chomp
+      puts "What is the student's country?"
+      country = gets.chomp
+      break
+      end
+    end
+    students << {name: name, cohort: cohort_string.to_sym,
+      hobby: hobby, country: country}
     puts "Now we have #{students.count} students"
     name = gets.chomp
   end
   students
 end
+
 def print_header
-  puts "The students of Villains Academy"
-  puts "--------------"
+  puts "The students of Villains Academy".center(50)
+  puts "--------------".center(50)
 end
 def print(students)
-  students.each.with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  counter = 0
+  while true do
+    hash = students[counter]
+    puts "#{counter + 1}. #{hash[:name]} "
+    puts "Hobbies: #{hash[:hobby]}, country: #{hash[:country]} "
+    puts "(Cohort #{hash[:cohort]})"
+    counter +=1
+    if counter == students.count
+      break
+    end
   end
 end
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(50)
 end
 students = input_students
 print_header
