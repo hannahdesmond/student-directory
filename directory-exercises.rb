6c2ced5f842b8cdbcfae4fcf7f7a9b9104f309f8
Hannah Desmond <desmond.hannah@googlemail.com>
2021-01-26 12:42:18 +0000

    Create method to save students into a new csv file.

--- directory-exercises.rb
+++ directory-exercises.rb
@@ -56,7 +56,7 @@ def print_students_list
   end
 end
 
-def print_footer(students)
+def print_footer
   if @students == nil
     puts "-----------".center(50)
   else
@@ -77,15 +77,18 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit"
 end
 
 def process(selection)
   case selection
     when "1"
-    # input the students
+      input_students
     when "2"
-    # show the students
+     show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -98,6 +101,18 @@ def interactive_menu
     print_menu
     process(gets.chomp)
   end
+end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
