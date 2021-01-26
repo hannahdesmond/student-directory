375491c790b1713becd747d2b20a8aaf24a126a7
Hannah Desmond <desmond.hannah@googlemail.com>
2021-01-26 15:53:14 +0000

    Add method for adding students to list. Load students from students.csv

--- directory-exercises.rb
+++ directory-exercises.rb
@@ -1,31 +1,51 @@
 @students = []
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit"
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+     show_students
+    when "3"
+      save_students
+    when "4"
+      load_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again."
+    end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  name = STDIN.gets.chomp
-  if name.empty?
-    puts "There are no students"
-  end
-  while !name.empty? do
-    while true do
-      puts "What is the student's cohort?"
-      cohort_string = gets.chomp
-      puts "Is this correct? #{cohort_string}? Put y or n"
-      answer = STDIN.gets.chomp
-      if answer == 'y'
-      break
-      end
-    end
-    @students << {name: name, cohort: cohort_string.to_sym}
+  @name = STDIN.gets.chomp
+  while !@name.empty? do
+    add_to_list
       if @students.count == 1
         puts "Now we have 1 student"
       elsif @students.count > 1
         puts "Now we have #{@students.count} students"
       end
       puts "What is the name of the next student?"
-      name = gets.delete("\n")
-    end
+      @name = STDIN.gets.chomp
+   end
 end
 
 def print_header
@@ -34,20 +54,8 @@ def print_header
 end
 
 def print_students_list
-  if @students == nil
-    puts "--------------".center(50)
-  else
-  students_array = []
-  puts "Which cohort do you want to print?"
-  input = STDIN.gets.chomp
-  @students.each do |student_hash|
-    student_hash.each do |key, value|
-      if key == :cohort && value == input.to_sym
-        students_array.push(student_hash[:name])
-      end
-    end
-  end
-  puts students_array.join(", ")
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
@@ -69,29 +77,8 @@ def show_students
   print_footer
 end
 
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit"
-end
-
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-     show_students
-    when "3"
-      save_students
-    when "4"
-      load_students
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again."
-    end
+def add_to_list
+  @students << {name: @name, cohort: :november}
 end
 
 def save_students
@@ -106,11 +93,11 @@ def save_students
   file.close
 end
 
-def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
+def load_students
+  file = File.open("students.csv", "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
-    @students << {name: name, cohort: cohort.to_sym}
+    add_to_list
   end
   file.close
 end
@@ -127,12 +114,6 @@ def try_load_students
   end
 end
 
-def interactive_menu
-  loop do
-    print_menu
-    process(STDIN.gets.chomp)
-  end
-end
 
 
 try_load_students
